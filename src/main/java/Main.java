92e358aaa67e3e8aab3d8bd7a26c255a9487654a
James Glover <3057283g@student.gla.ac.uk>
2025-04-09 13:04:43 +0100

    Crude but efficient solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,18 @@ public class Main {
 }
 
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int insertionPoint = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                continue;
-            }
-            nums[insertionPoint] = nums[i];
-            insertionPoint++;
+    public int[] getConcatenation(int[] nums) {
+        int newSize = nums.length * 2;
+        int[] ans = new int[newSize];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
         }
-        return insertionPoint;
+
+        for (int i = nums.length; i < newSize; i++) {
+            ans[i] = nums[i -nums.length];
+        }
+
+        return ans;
     }
 }

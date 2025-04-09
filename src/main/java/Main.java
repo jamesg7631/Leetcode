f6fa1cb8eaa8e144ef9925e9c3f1f49f8aa34c35
James Glover <3057283g@student.gla.ac.uk>
2025-04-09 13:08:52 +0100

    Empty main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,4 @@ public class Main {
 }
 
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int insertionPoint = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                continue;
-            }
-            nums[insertionPoint] = nums[i];
-            insertionPoint++;
-        }
-        return insertionPoint;
-    }
 }

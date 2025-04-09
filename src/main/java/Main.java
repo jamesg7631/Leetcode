6f7d13c865d22da9462a3ec3936f1460fc51b388
James Glover <3057283g@student.gla.ac.uk>
2025-04-09 15:50:46 +0100

    Minstack attempt. Passed test cases

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,45 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+}
+
+class MinStack {
+    private LinkedList<Integer> stack;
+    private LinkedList<Integer> minStack;
+    public MinStack() {
+        this.stack = new LinkedList<>();
+        this.minStack = new LinkedList<>();
+    }
+
+    public void push(int val) {
+        stack.addFirst(val);
+        if (minStack.isEmpty()) {
+            minStack.addFirst(val);
+        } else {
+            int peek = minStack.peekFirst();
+            if (val < peek) {
+                minStack.addFirst(val);
+            } else {
+                minStack.addFirst(peek);
+            }
+        }
+    }
+
+    public void pop() {
+        stack.removeFirst();
+        minStack.removeFirst();
+    }
+
+    public int top() {
+        int top = stack.peekFirst();
+        return top;
+    }
+
+    public int getMin() {
+        return minStack.peekFirst();
     }
 }
 

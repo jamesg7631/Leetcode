90474476a7fd1453ed33da8125908bdbcbe5c825
James Glover <3057283g@student.gla.ac.uk>
2025-04-09 12:27:50 +0100

    Initital Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int insertionPoint = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+            nums[insertionPoint] = nums[i];
+            insertionPoint++;
+        }
+        return insertionPoint;
+    }
+}

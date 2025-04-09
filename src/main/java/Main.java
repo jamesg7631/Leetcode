c73acf0ab2365bfd240f6166c147be04a89ec98d
James Glover <3057283g@student.gla.ac.uk>
2025-04-09 12:55:16 +0100

    My solution. Very efficient

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+//        int[] nums = {3,2,2,3};
+        int[] nums = {0,1,2,2,3,0,4,2};
+        System.out.println(sol.removeElement(nums, 2));
+        System.out.println(Arrays.toString(nums));
     }
 }
 
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int insertionPoint = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                continue;
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
             }
-            nums[insertionPoint] = nums[i];
-            insertionPoint++;
         }
-        return insertionPoint;
+        return k;
     }
 }

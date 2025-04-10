4b2c40665691081960809d7062dcbe0ae8c25ad5
James Glover <3057283g@student.gla.ac.uk>
2025-04-10 10:44:28 +0100

    Contains Duplicate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[] nums = {1, 2, 3, 4};
+        Solution sol = new Solution();
+        System.out.println(sol.hasDuplicate(nums));
     }
 }
 
 class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> uniqueNums = new HashSet<>();
+        for (Integer num: nums) {
+            if (uniqueNums.contains(num)) {
+                return true;
+            }
+            uniqueNums.add(num);
+        }
+        return false;
+    }
 }

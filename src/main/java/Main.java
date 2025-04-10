af77e93df78cb8c9493af960bc16dbd2b0b47cd2
James Glover <3057283g@student.gla.ac.uk>
2025-04-10 10:55:59 +0100

    Valid anagram

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        String s = "jar";
+        String t = "jam";
+        System.out.println(sol.isAnagram(s,t));
     }
 }
 
 class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] numbers = new int[26];
+        for (char c: s.toCharArray()) {
+            int index = c - 'a';
+            numbers[index] += 1;
+        }
+
+        for(char c: t.toCharArray()) {
+            int index = c - 'a';
+            numbers[index] -= 1;
+            if (numbers[index] < 0) {
+                return false;
+            }
+        }
+
+        for (int num: numbers) {
+            if (num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }

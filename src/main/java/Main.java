6cac74ddb30def434c2f22ab43d4fa599d6c4a5b
James Glover <3057283g@student.gla.ac.uk>
2025-04-11 15:38:07 +0100

    Beats 75.64%

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,47 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        // Test Case 1
+//        String[] tokens = {"2", "1", "+", "3", "*"};
+        // Test Case 2
+//        String[] tokens = {"4", "13", "5", "/", "+"};
+        // Test Case 3
+        String[] tokens = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+        System.out.println(sol.evalRPN(tokens));
     }
 }
 
 class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                int number1 = stack.pop();
+                int number2 = stack.pop();
+                int sum = number1 + number2;
+                stack.push(sum);
+            } else if (token.equals("-")) {
+                int number1 = stack.pop();
+                int number2 = stack.pop();
+                int difference = number2 - number1;
+                stack.push(difference);
+            } else if (token.equals("*")) {
+                int number1 = stack.pop();
+                int number2 = stack.pop();
+                int result = number1 * number2;
+                stack.push(result);
+            } else if (token.equals("/")) {
+                int number1 = stack.pop();
+                int number2 = stack.pop();
+                int result = number2 / number1;
+                stack.push(result);
+            } else {
+                int number = Integer.parseInt(token);
+                stack.push(number);
+            }
+        }
+        return stack.pop();
+    }
 }

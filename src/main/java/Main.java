705392bb3896b70aa1af9d1e3255a9b3fbca53d0
James Glover <3057283g@student.gla.ac.uk>
2025-04-11 15:08:25 +0100

    Two Sum with time complexity O(n) and space complexity O(1)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,41 +6,32 @@ public class Main {
         //Test Case 1
 //        int[] nums = {1,2,3,4};
         // Test Case 2
-//        int[] nums = {2, 7, 11, 15};
+        int[] nums = {2, 7, 11, 15};
         // Test Case 3
 //        int[] nums = {2, 3, 4};
 //        // Test Case 4
 //        int[] nums = {-1, 0};
         // Test Case 5
-        int[] nums = {-10, -8, -2, 1, 2, 5, 6};
+//        int[] nums = {-10, -8, -2, 1, 2, 5, 6};
         Solution sol = new Solution();
-        System.out.println(Arrays.toString(sol.twoSum(nums, 0)));
+        System.out.println(Arrays.toString(sol.twoSum(nums, 9)));
     }
 }
 
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int pointer1 = 0;
-        int pointer2 = 1;
-
-        while (pointer1 < numbers.length - 1) {
-            int sum = numbers[pointer1] + numbers[pointer2];
+        int left = 0;
+        int right = numbers.length -1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
             if (sum == target) {
-                int[] result = new int[2];
-                result[0] = pointer1 + 1;
-                result[1] = pointer2 + 1;
-                return result;
-            } else if (sum > target) {
-                pointer1++;
-                pointer2 = pointer1 + 1;
+                return new int[] {(left + 1), (right + 1)};
+            } else if (sum < target) {
+                left++;
             } else {
-                pointer2++;
-                if (pointer2 >= numbers.length) {
-                    pointer1++;
-                    pointer2 = pointer1 + 1;
-                }
+                right--;
             }
         }
-        return numbers;
+        throw new RuntimeException("Total is never reached");
     }
 }

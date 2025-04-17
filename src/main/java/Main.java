2a32ac27bf46188ed5e82f94e376e904514b7f2c
James Glover <3057283g@student.gla.ac.uk>
2025-04-17 15:44:44 +0100

    Easy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,45 @@
+import com.sun.source.tree.Tree;
+
 public class Main {
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        // Test Case 1
+        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7));
+//        TreeNode found = sol.searchBST(root, 2);
+        // Test Case 2
+        TreeNode found = sol.searchBST(root, 5);
+        System.out.println();
     }
 }
 
 class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (val == root.val) {
+            return root;
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }

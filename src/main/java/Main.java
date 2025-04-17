ee46c38a51a5bf458bdf7f15fd80b126f215bd89
James Glover <3057283g@student.gla.ac.uk>
2025-04-17 11:27:01 +0100

    O(n) maxSubarray

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Test Case 1
+//        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        // Test Case 2
+//        int[] nums = {1};
+        // Test Case 3
+        int[] nums = {5,4,-1,7,8};
+        Solution sol = new Solution();
+        System.out.println(sol.maxSubArray(nums));
     }
 }
 
 class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int curSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            curSum = Math.max(0, curSum);
+            curSum += nums[i];
+            maxSum = Math.max(curSum, maxSum);
+        }
+        return maxSum;
+    }
 }

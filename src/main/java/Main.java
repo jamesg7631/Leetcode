28d0a4691dd89cf5c2123b038afa655c3d437de6
James Glover <3057283g@student.gla.ac.uk>
2025-04-17 16:38:06 +0100

    Easy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(4,new TreeNode(2, new TreeNode(1),new TreeNode(3)), new TreeNode(7));
+        TreeNode returnedNode = solution.insertIntoBST(root, 5);
+        System.out.println();
     }
 }
 
 class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
+        }
+
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
